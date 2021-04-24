725b2bf6b5da15b1de785112db58a80687db60b6
Jiri Otoupal <jiri.otoupal@uptim.ai>
2021-04-24 21:25:56 +0200

    Matrix metoda

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import PIL
+import numpy
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+from PIL.Image import Image
 
+
+def image_to_matrix():
+    frames_path = ".jpg"
+    img = Image.open(frames_path)
+    data = np.array(img)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
