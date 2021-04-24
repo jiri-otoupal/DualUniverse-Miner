dd27654f417920164a17e4c6ca3e34f51653929a
Jiri Otoupal <jiri.otoupal@uptim.ai>
2021-04-24 23:49:26 +0200

    weighting

--- main.py
+++ main.py
@@ -14,14 +14,16 @@ def image_to_matrix(path: str):
     data = np.array(img)
     return data
 
-def image_weight(data):
-    chunk_weights = np.isin(data[:,:,0],np.array_split(data[:,0:4,0],8)[0])
+
+def image_weight(data: np.matrix, rgb: int, chunk_no: int):
+    chunk_weights = np.isin(data[:, :, rgb], np.array_split(data[:, chunk_no:chunk_no + 4, rgb], 8)[chunk_no])
     num_of_true = np.sum(chunk_weights)
     comp_size = np.size(chunk_weights)
     return num_of_true / comp_size
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-   print()
+    print()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
