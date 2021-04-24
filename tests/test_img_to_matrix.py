c2810001aa1043d693c1a35d1983c2307911fcce
domin <dominik.deak@seznam.cz>
2021-04-24 22:14:30 +0200

    a

--- tests/test_img_to_matrix.py
+++ tests/test_img_to_matrix.py
@@ -5,7 +5,6 @@ from main import image_to_matrix
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
         img = image_to_matrix("../images/bauxite/l.jpg")
         pass
 
