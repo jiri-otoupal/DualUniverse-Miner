f831cabd6a6b41e980b4074526cbf89fa75f2631
domin <dominik.deak@seznam.cz>
2021-04-26 11:36:19 +0200

    a

--- tests/test_img_to_matrix.py
+++ tests/test_img_to_matrix.py
@@ -1,9 +1,4 @@
-import struct
 import unittest
-import numpy as np
-from PIL import Image
-from pyocr import pyocr
-from pytesseract import pytesseract
 
 from main import image_to_matrix, image_weight_by_color, image_recognize_by_color, intersect_colors, image_recognize, \
     image_weight, compare_to_all_samples, apply_filter, img_frombytes, apply_filter_dilation
@@ -12,20 +7,21 @@ from main import image_to_matrix, image_weight_by_color, image_recognize_by_colo
 class MyTestCase(unittest.TestCase):
 
     def test_something(self):
-        imgs = image_to_matrix("../images/samples/fullhdlacobus.png")
-        #tools = pyocr.get_available_tools()
-        #langs = tools[0].get_available_languages()
-        #digits = tools[0].image_to_string(
+        imgs = image_to_matrix("../images/samples/lacobus2.png")
+        # tools = pyocr.get_available_tools()
+        # langs = tools[0].get_available_languages()
+        # digits = tools[0].image_to_string(
         #    Image.open("../images/samples/fullhdlacobus.png"),
         #    lang=langs[0],
         #    builder=pyocr.tesseract.DigitBuilder()
-        #)
-        #print(digits)
+        # )
+        # print(digits)
 
-        rec = compare_to_all_samples(imgs)
-        mat = apply_filter(rec, 25)
-        img_frombytes(mat).show()
+        rec = compare_to_all_samples(imgs, False)
+        mat = apply_filter(rec, 60)
+        c = apply_filter_dilation(mat, 1)
+        img_frombytes(c).show()
 
 
-if __name__ == '__main__':
+if __name__ == 'main':
     unittest.main()
