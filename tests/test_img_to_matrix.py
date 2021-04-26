1d804e65d98b48c3240e2d4568b3d88f34e1fcfe
Jiri Otoupal <jiri.otoupal@uptim.ai>
2021-04-26 12:46:52 +0200

    changes

--- tests/test_img_to_matrix.py
+++ tests/test_img_to_matrix.py
@@ -1,4 +1,12 @@
+from threading import Thread
+
+import cv2
 import unittest
+
+import numpy as np
+import pyautogui
+from matplotlib import pyplot as plt
+from numpy.lib.stride_tricks import as_strided
 
 from main import image_to_matrix, image_weight_by_color, image_recognize_by_color, intersect_colors, image_recognize, \
     image_weight, compare_to_all_samples, apply_filter, img_frombytes, apply_filter_dilation
@@ -18,7 +26,7 @@ class MyTestCase(unittest.TestCase):
         # print(digits)
 
         rec = compare_to_all_samples(imgs, False)
-        mat = apply_filter(rec, 60)
+        mat = apply_filter(rec, 25)
         c = apply_filter_dilation(mat, 1)
         img_frombytes(c).show()
 
