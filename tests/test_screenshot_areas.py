3611a21ed2f489cab08c6962156e186c5915887b
Jiri Otoupal <jiri.otoupal@uptim.ai>
2021-05-02 00:32:08 +0200

    changes

--- tests/test_screenshot_areas.py
+++ tests/test_screenshot_areas.py
@@ -11,8 +11,8 @@ class MyTestCase(unittest.TestCase):
     def test_areas(self):
         # TODO: pass function that will be called if is too far away and if see ore
         sleep(3)
-        my = pygetwindow.getWindowsWithTitle("DualBot")[0]
-        vision = Vision(None, None, None)
+        my = pygetwindow.getWindowsWithTitle("Dual Universe")[0]
+        vision = Vision(my, None, None)
         pyautogui.screenshot("left.png", region=vision.get_left_area())
         pyautogui.screenshot("right.png", region=vision.get_right_area())
         pyautogui.screenshot("top.png", region=vision.get_top_area())
