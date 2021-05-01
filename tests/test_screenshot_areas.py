4fe1dbcc1a1cdb5b7287ff347688c4411074a719
Jiri Otoupal <jiri.otoupal@uptim.ai>
2021-05-02 00:17:41 +0200

    changes

--- tests/test_screenshot_areas.py
+++ tests/test_screenshot_areas.py
@@ -1,5 +1,6 @@
 import unittest
 
+import pyautogui
 import pygetwindow
 
 from vision import Vision
@@ -10,7 +11,10 @@ class MyTestCase(unittest.TestCase):
         # TODO: pass function that will be called if is too far away and if see ore
         my = pygetwindow.getWindowsWithTitle("DualBot")[0]
         vision = Vision(my, None, None)
-        vision.what_is_in_area(vision.get_left_area())
+        pyautogui.screenshot("left.png", region=vision.get_left_area())
+        pyautogui.screenshot("right.png", region=vision.get_right_area())
+        pyautogui.screenshot("top.png", region=vision.get_top_area())
+        pyautogui.screenshot("bottom.png", region=vision.get_bottom_area())
 
 
 if __name__ == '__main__':
