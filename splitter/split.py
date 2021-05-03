9c8bc9192dad99cc6d13b92ad6fc418fbcfee601
Jiri Otoupal <jiri.otoupal@uptim.ai>
2021-05-03 20:35:01 +0200

    changes

--- splitter/split.py
+++ splitter/split.py
@@ -11,18 +11,21 @@ def tile(filename, dir_in, dir_out, d):
     img = Image.open(os.path.join(dir_in, filename))
     img = img.convert("RGB")
     w, h = img.size
-    ran = randint(0, 999999999)
+    ran = randint(-1212464564, 9999999999)
 
     grid = list(product(range(0, h - h % d, d), range(0, w - w % d, d)))
     for i, j in grid:
         box = (j, i, j + d, i + d)
-        out = os.path.join(dir_out, f'{name}_{i}_{j}{ran}.jpg')
+        out = os.path.join(dir_out, f'out_{i}_{j}{ran}.jpg')
         img.crop(box).save(out)
 
 
-tile("terrain.png", "", "", 32)
-os.remove("terrain.png")
-
+print("Cropping")
+for file in os.listdir("."):
+    if "obrazovky" in file or "screen" in file or "Dual Universe" in file:
+        tile(file, "", "", 32)
+        os.remove(file)
+print("Removing invalid sizes")
 for imgy in os.listdir("."):
     img = cv2.imread(imgy, cv2.IMREAD_GRAYSCALE)
     if img is None:
