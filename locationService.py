fd9d3b7984d5ddb5209c99f73369b4c2ae5ee4c8
Jiri Otoupal <jiri.otoupal@uptim.ai>
2021-05-05 15:55:43 +0200

    Error fix

--- locationService.py
+++ locationService.py
@@ -8,6 +8,11 @@ class LocationService:
 
     def __init__(self):
         pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+        self.speed = None
+        self.height = None
+        self.location_x = None
+        self.location_y = None
+        self.location_z = None
 
     def predict_numbers(self, path):
         image = cv2.imread(os.path.normpath(path), 0)
@@ -19,3 +24,15 @@ class LocationService:
         data = pytesseract.image_to_string(result, lang='eng', config='--psm 10')
         processed_data = ''.join(char for char in data if char.isnumeric() or char == '.' or char == "-")
         return processed_data
+
+    def get_map_location(self):
+        # TODO
+        pass
+
+    def get_speed(self):
+        # TODO
+        pass
+
+    def get_height(self):
+        # TODO
+        pass
